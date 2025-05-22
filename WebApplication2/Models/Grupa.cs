4b31ad482114c9d4ad6e3e2ffc505e50d7928b20
Branko Kovacevic <branko.kovacevic09@gmail.com>
2025-05-22 11:56:53 +0200

    Kreiranje modela za Korisnika i Grupu

---
+++ WebApplication2/Models/Grupa.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApplication2.Models
+{
+    public class Grupa
+    {
+        public int Id { get; set; }
+        public string Ime { get; set; }
+        public DateTime datumOsnivanja { get; set; }
+
+        public List<Korisnik> Korisnici { get; set; } = new List<Korisnik>();
+
+        public Grupa(int id, string ime, DateTime datumOsnivanja)
+        {
+            Id = id;
+            Ime = ime;
+            this.datumOsnivanja = datumOsnivanja;
+        }
+    }
+}
---
+++ WebApplication2/Models/Korisnik.cs
@@ -0,0 +1,22 @@
+﻿namespace WebApplication2.Models
+{
+    public class Korisnik
+    {
+        public int Id { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public DateTime Datum { get; set; }
+
+        public List<Grupa> Grupe { get; set; } = new List<Grupa>();
+
+        public Korisnik(int id, string ime, string prezime, DateTime datum)
+        {
+            Id = id;
+            Ime = ime;
+            Prezime = prezime;
+            Datum = datum;
+        }
+    }
+
+    
+}
