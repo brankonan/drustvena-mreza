45c490327c287a031379cc43565529c71d8d0166
AnaRadmanovic22 <anaradmanovic21@gmail.com>
2025-06-18 14:46:41 +0200

    Implementirana metoda za dobavljanje svih podataka

--- WebApplication2/Controllers/GrupaController.cs
+++ WebApplication2/Controllers/GrupaController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
 using WebApplication2.Models;
 using WebApplication2.Repositories;
 
@@ -12,14 +13,68 @@ namespace WebApplication2.Controllers
     {
         public GrupaRepository grupaRepository = new GrupaRepository();
 
+        private string connectionString = "Data Source=database/mydatabase.db";
 
+
+        private List<Grupa> GetAllFromDataBase()
+        {
+
+            List<Grupa> grupe = new List<Grupa>();
+
+            try
+            {
+                using SqliteConnection connection = new SqliteConnection(connectionString);
+                connection.Open();
+
+                string query = "SELECT Id, Name, CreationDate FROM Groups";
+                using SqliteCommand command = new SqliteCommand(query, connection);
+
+                using SqliteDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    int id = Convert.ToInt32(reader["Id"]);
+                    string ime = reader["Name"].ToString();
+                    DateTime datum = Convert.ToDateTime(reader["CreationDate"]);
+
+                    Grupa g = new Grupa(id, ime, datum);
+                    grupe.Add(g);
+                }
+            
+            }
+            catch (SqliteException ex)
+            {
+                Console.WriteLine($"Greška pri konekciji ili izvršavanju neispravnih SQL upita: {ex.Message}");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine($"Greška u konverziji podataka iz baze: {ex.Message}");
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine($"Konekcija nije otvorena ili je otvorena više puta: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Neočekivana greška: {ex.Message}");
+            }
+
+            return grupe;
+        }
 
         [HttpGet]
         public ActionResult<List<Grupa>> GetAll()
         {
-            List<Grupa> grupe = GrupaRepository.Data.Values.ToList();
-            return Ok(grupe);
+            try
+            {
+                List<Grupa> grupe = GetAllFromDataBase();
+                return Ok(grupe);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Greška prilikom dobavljanja podataka: {ex.Message}");
+            }
         }
+
 
 
         [HttpPost]
